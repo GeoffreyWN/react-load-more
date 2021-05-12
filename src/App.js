89be98e87f5c45749217684ebf8edd05dffbb992
geof <>
2021-05-12 11:00:07 +0300

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import "./App.css";
 import axios from "axios";
